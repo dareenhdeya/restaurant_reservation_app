650f96ddd2ce8618db165768af430662fa47edd5
dareen <dareenhadeya@gmail.com>
2025-12-15 01:04:06 +0200

    edit main

--- customer_app/lib/main.dart
+++ customer_app/lib/main.dart
@@ -1,24 +1,31 @@
 import 'package:customer_app/auth/auth_wrapper.dart';
+import 'package:customer_app/auth/cubit/auth_cubit.dart';
+import 'package:customer_app/auth/cubit/auth_state.dart';
 import 'package:customer_app/auth/screens/login_screen.dart';
 import 'package:customer_app/home/home_screen.dart';
 import 'package:customer_app/providers/booking_provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:customer_app/providers/restaurant_provider.dart';
+import 'package:customer_app/auth/services/auth_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:customer_app/firebase_options.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
-import 'providers/restaurant_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => RestaurantProvider()),
         ChangeNotifierProvider(create: (_) => BookingProvider()),
       ],
-      child: const MyApp(),
+      child: BlocProvider(
+        create: (_) => AuthCubit(AuthService()),
+        child: const MyApp(),
+      ),
     ),
   );
 }
@@ -26,29 +33,13 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Customer App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
 
       home: const AuthWrapper(),
